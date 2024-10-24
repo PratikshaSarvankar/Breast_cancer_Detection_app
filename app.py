a247e872d65016c607235f95c6cdce77c9c595dd
PratikshaSarvankar <159712961+PratikshaSarvankar@users.noreply.github.com>
2024-10-24 20:04:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def first_page():
         col1, col2 = st.columns(2)
 
         # Load the image
-        image_path = r"C:\Users\Pratiksha\Desktop\BreastCancerPredictionApp\1stpage.jpeg"  # Ensure the path is correct
+        image_path = r"1stpage.jpeg"  # Ensure the path is correct
         img = Image.open(image_path)
 
         # Display the image in the first column, resized
@@ -150,7 +150,7 @@ def second_page():
         col1, col2 = st.columns(2)
 
         # Load the image
-        image_path = r"C:\Users\Pratiksha\Desktop\BreastCancerPredictionApp\2ndpage.jpg"  # Ensure the path is correct
+        image_path = r"2ndpage.jpg"  # Ensure the path is correct
         img = Image.open(image_path)
 
         # Display the image in the first column, maintaining aspect ratio
